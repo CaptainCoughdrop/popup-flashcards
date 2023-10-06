905a21f023bab844b4ca570c716727b6e9836cee
CaptainCoughdrop <98195323+CaptainCoughdrop@users.noreply.github.com>
2023-10-06 17:19:49 -0600

    Chooses a random q and a from a text file

--- popup-flashcards.py
+++ popup-flashcards.py
@@ -1,27 +1,30 @@
 import time
 import random
 from tkinter import simpledialog
+import json
+
+with open('questions.txt') as f:
+    data = f.read()
+questions = json.loads(data)
 
 def ask_question(question, answer):
     while True:
         item = simpledialog.askstring("Question", question)
         if item is None:
             exit()
+        if item == "dict":
+            for i in questions.items():
+                for x in i:
+                    print(x)
         if item == answer:
             print(item)
             break
-    time.sleep(3)
+    time.sleep(1)
     
 
 while True:
     while True:
-        x = random.randint(0, 2)
-
-        if x == 0:
-            ask_question("Type 'a':", "a")
-    
-        if x == 1:
-            ask_question("Type 'b':", "b")
-
-        if x == 2:
-            ask_question("Type 'c':", "c")
+        questions_list = list(questions.items())
+        random_question = random.choice(questions_list)
+        ques, ans = random_question
+        ask_question(ques, ans)

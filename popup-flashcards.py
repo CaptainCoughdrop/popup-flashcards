c973f94e1fb4d7d4bbbbf2f8cf4708d9f40bedd3
CaptainCoughdrop <98195323+CaptainCoughdrop@users.noreply.github.com>
2023-10-05 21:44:59 -0600

    Add question function

--- popup-flashcards.py
+++ popup-flashcards.py
@@ -2,45 +2,26 @@ import time
 import random
 from tkinter import simpledialog
 
+def ask_question(question, answer):
+    while True:
+        item = simpledialog.askstring("Question", question)
+        if item is None:
+            exit()
+        if item == answer:
+            print(item)
+            break
+    time.sleep(3)
+    
+
 while True:
     while True:
         x = random.randint(0, 2)
 
         if x == 0:
-            item = simpledialog.askstring("Question", "Type 'a':")
-            while True:
-                if item == "stop":
-                    exit()
-                if item == "a":
-                    print(item)
-                    break
-                else:
-                    item = simpledialog.askstring("Question", "Type 'a':")
-            time.sleep(3)
-            break
+            ask_question("Type 'a':", "a")
     
         if x == 1:
-            item = simpledialog.askstring("Question", "Type 'b':")
-            while True:
-                if item == "stop":
-                    exit()
-                if item == "b":
-                    print(item)
-                    break
-                else:
-                    item = simpledialog.askstring("Question", "Type 'b':")
-            time.sleep(3)
-            break
+            ask_question("Type 'b':", "b")
 
         if x == 2:
-            item = simpledialog.askstring("Question", "Type 'c':")
-            while True:
-                if item == "stop":
-                    exit()
-                if item == "c":
-                    print(item)
-                    break
-                else:
-                    item = simpledialog.askstring("Question", "Type 'c':")
-            time.sleep(3)
-            break
+            ask_question("Type 'c':", "c")
